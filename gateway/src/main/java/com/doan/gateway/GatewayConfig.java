050f2de474a2693f2f8641ad5bcdb2ae0467d2b7
nhinguyen1011 <75206214+nhinguyen1011@users.noreply.github.com>
2024-09-25 10:27:20 +0700

    Add files via upload

---
+++ gateway/src/main/java/com/doan/gateway/GatewayConfig.java
@@ -0,0 +1,18 @@
+package com.doan.gateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("employee-service", r -> r.path("/employee/**")
+                        .uri("lb://employee-service"))
+                .build();
+    }
+}
